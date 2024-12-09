6f8182fcefe595a6de0da55b9b366ac1c1f626af
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-09 18:42:51 +0530

    AutoBoxing

---
+++ Java/src/wrapperClass/AutoBoxing.java
@@ -0,0 +1,27 @@
+package wrapperClass;
+
+public class AutoBoxing {
+
+	public static void main(String[] args) {
+		Byte a=19;
+		Short c=12;
+		Integer b=10;
+		Long d=19089l;
+		Float e=10.8f;
+		Double s=123.0;
+		Character v='s';
+		Boolean m=true;
+		System.out.println("Non-primitive Date types is :"+a);
+		System.out.println("Non-primitive Date types is :"+b);
+		System.out.println("Non-primitive Date types is :"+c);
+		System.out.println("Non-primitive Date types is :"+d);
+		System.out.println("Non-primitive Date types is :"+e);
+		System.out.println("Non-primitive Date types is :"+s);
+		System.out.println("Non-primitive Date types is :"+m);
+		System.out.println("Non-primitive Date types is :"+v);
+		
+		
+
+	}
+
+}
