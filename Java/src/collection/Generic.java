8b40b3f230fb49afdf03a6b9d37f7577c5849627
santhoshE429 <santhoshelumalaim@gmail.com>
2024-12-17 18:23:10 +0530

    Generic

---
+++ Java/src/collection/Generic.java
@@ -0,0 +1,34 @@
+package collection;
+
+import java.util.ArrayList;
+import java.util.List;
+
+class Student2
+{
+	int sid;
+	String sname;
+	public Student2(int sid, String sname) {
+		super();
+		this.sid = sid;
+		this.sname = sname;
+	}
+	@Override
+	public String toString() {
+		return "Id-"+sid+":Sname-"+sname;
+	}
+}
+public class Generic {
+
+	public static void main(String[] args) {
+		List <Student2> ls=new ArrayList();
+		ls.add(new Student2(1,"Sant"));
+		ls.add(new Student2(2,"xyz"));
+		ls.add(new Student2(3,"Abc"));
+		ls.add(null);
+		ls.add(1, new Student2(8, "Tabrez"));
+		
+		System.out.println(ls);
+
+	}
+
+}
